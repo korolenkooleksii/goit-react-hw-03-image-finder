a13d9bc3b0d0d455a868c20e82ea0c3e1280f420
Oleksii Korolenko <as_vostok@i.ua>
2023-03-29 15:11:54 +0300

    start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {}
+
+  componentDidUpdate(prevProps, prevState) {}
+
+  render() {
+    // const { ,  } = this.state;
+    return <></>;
+  }
+}
+
+export default App;
+console.log(555);
